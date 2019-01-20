b0bc8733d3f65fd613b9aca3066032332a4a5c3f
Kyle Scheuerlein <kyle@kylesureline.com>
2019-01-19 21:52:36 -0800

    Add event listener to qwerty keys

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const overlay = document.querySelector('#overlay');
 const startGame = document.querySelector('.btn__reset');
+const qwerty = document.querySelector('#qwerty');
 
 startGame.addEventListener('click', (e) => {
   overlay.style.display = 'none';
+});
+
+qwerty.addEventListener('click', (e) => {
+  if(e.target.tagName === 'BUTTON') {
+    // alert(e.target.textContent);
+    e.target.style.display = 'none';
+  }
 });
